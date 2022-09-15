23966788b22780e84069182b020915c763c16ab1
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2022-09-15 20:39:15 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main() //initialising the main function
+{
+printf("Hello World"); //keyword to print a statement
+return 0; //as the main function is of int type, then return value should also be of int type
+}
