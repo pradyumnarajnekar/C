d86f1ef92653740f251e29b14abf133a6c1a515e
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2022-10-05 17:39:05 +0530

    Delete from end CSLL

--- delete_from_end_cir_sing_link_list.c
+++ delete_from_end_cir_sing_link_list.c
@@ -7,18 +7,18 @@ struct node
 };
 int main()
 {
-    struct node *head, *node1, *temporary1, *temporary2, *temporary3, *temporary4;
+    struct node *head_start, *node1, *temporary1, *temporary2, *temporary3, *temporary4;
     int n, i, data;
     printf("Enter the number of nodes : ");
     scanf("%d",&n);
     if(n>=1)
     {
-        head=(struct node *) malloc(sizeof(struct node));
+        head_start=(struct node *) malloc(sizeof(struct node));
         printf("\nEnter the data of node 1 : ");
         scanf("%d",&data);
-        head->data=data;
-        head->next=NULL;
-        temporary1=head;
+        head_start->data=data;
+        head_start->next=NULL;
+        temporary1=head_start;
         for(i=2;i<=n;i++)
         {
             node1=(struct node *) malloc(sizeof(struct node));
@@ -29,15 +29,15 @@ int main()
             temporary1->next=node1;
             temporary1=node1;
         }
-        temporary1->next=head;
+        temporary1->next=head_start;
     }
-    if(head==NULL)
+    if(head_start==NULL)
     {
         printf("List is empty");
     }
     else
     {
-        temporary4=head;
+        temporary4=head_start;
         printf("\nThe data entere in the list is : \n");
         do
         {
@@ -45,24 +45,24 @@ int main()
             temporary4=temporary4->next;
             n++;
         }
-        while(temporary4!=head);
+        while(temporary4!=head_start);
     }
-    temporary2=head;
-    while(temporary2->next!=head)
+    temporary2=head_start;
+    while(temporary2->next!=head_start)
     {
         temporary3=temporary2;
         temporary2=temporary2->next;
     }
-    temporary3->next=head;
+    temporary3->next=head_start;
     free(temporary2);
     n=1;
-    if(head==NULL)
+    if(head_start==NULL)
     {
         printf("List is empty\n");
     }
     else
     {
-        temporary4=head;
+        temporary4=head_start;
         printf("\nAfter deleting node from the end, the circular linked list is : \n");
         do
         {
@@ -70,7 +70,7 @@ int main()
             temporary4=temporary4->next;
             n++;
         }
-        while(temporary4!=head);
+        while(temporary4!=head-start);
     }
     return 0;
 }
