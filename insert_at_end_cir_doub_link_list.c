ce92425d680d8001c8b29787c397a7502b060d88
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2022-10-06 17:25:20 +0530

    Insert at end CDLL

---
+++ insert_at_end_cir_doub_link_list.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+ struct node *previous;
+ int data;
+ struct node *next;
+};
+int main()
+{
+    struct node *head_start, *node1, *new_node_at_end, *temporary1, *temporary2;
+    int n, i, data, new_data_for_new_node;
+    printf("Enter the number of nodes : ");
+    scanf("%d",&n);
+    if(n>=1)
+    {
+        head_start=(struct node *) malloc(sizeof(struct node));
+        printf("\nEnter the data for node 1 : ");
+        scanf("%d",&data);
+        head_start->data=data;
+        head_start->next=head_start;
+        head_start->previous=head_start;
+        temporary1=head_start;
+        for(i=2;i<=n;i++)
+        {
+            node1=(struct node *)malloc(sizeof(struct node));
+            printf("\nEnter the data for node %d : ",i);
+            scanf("%d",&data);
+            node1->data=data;
+            node1->next=head_start;
+            node1->previous=temporary1;
+            temporary1->next=node1;
+            temporary1=node1;
+        }
+        temporary1->next=head_start;
+    }
+    n=1;
+    temporary2=head_start;
+    printf("\nThe Circular Doubly Linked List is : \n");
+    do
+    {
+        printf(" <= %d => ",temporary2->data);
+        temporary2=temporary2->next;
+        n++;
+    }
+    while(temporary2!=head_start);
+    printf("\nEnter the data for the node  to be inserted at end : ");
+    scanf("%d", &new_data_for_new_node);
+    new_node_at_end=(struct node *) malloc(sizeof(struct node));
+    new_node_at_end->data=new_data_for_new_node;
+    new_node_at_end->next=head_start;
+    new_node_at_end->previous=temporary1;
+    head_start->previous=new_node_at_end;
+    temporary1->next=new_node_at_end;
+    temporary1=new_node_at_end;
+    n=1;
+    temporary2=head_start;
+    printf("\nAfter insertion at end, the Circular Doubly Linked List is : \n");
+    do
+    {
+        printf(" <= %d => ",temporary2->data);
+        temporary2=temporary2->next;
+        n++;
+    }
+    while(temporary2!=head_start);
+    return 0;
+}
