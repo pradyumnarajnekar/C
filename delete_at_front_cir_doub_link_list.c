3b62d1a5f54b62548b84e6170d8c13ce638f8b6e
प्रद्युम्न राज़नेकर <112924476+pradyumnarajnekar@users.noreply.github.com>
2022-10-06 18:23:42 +0530

    Delete from front CDLL

---
+++ delete_at_front_cir_doub_link_list.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+ struct node *temporary3vious;
+ int data;
+ struct node *next;
+};
+int main()
+{
+    struct node *head_start, *node1,*temporary1, *temporary2, *temporary3, *temporary4;
+    int n, i, data;
+    printf("Enter the number of nodes : ");
+    scanf("%d",&n);
+    if(n>=1)
+    {
+        head_start=(struct node *) malloc(sizeof(struct node));
+        printf("Enter the data for node 1 : ");
+        scanf("%d",&data);
+        head_start->data=data;
+        head_start->next=head_start;
+        head_start->temporary3vious=head_start;
+        temporary1=head_start;
+        for(i=2;i<=n;i++)
+        {
+            node1=(struct node *)malloc(sizeof(struct node));
+            printf("Enter the data for node %d : ",i);
+            scanf("%d",&data);
+            node1->data=data;
+            node1->next=head_start;
+            node1->temporary3vious=temporary1;
+            temporary1->next=node1;
+            temporary1=node1;
+        }
+        temporary1->next=head_start;
+    }
+    n=1;
+    temporary4=head_start;
+    printf("\nThe data entered in the list is : \n");
+    do
+    {
+        printf(" <= %d =>",temporary4->data);
+        temporary4=temporary4->next;
+        n++;
+    }
+    while(temporary4!=head_start);
+    temporary2=head_start;
+    while(temporary2->next!=head_start)
+    {
+        temporary2=temporary2->next;
+    }
+    temporary3=head_start;
+    head_start=head_start->next;
+    head_start->temporary3vious=temporary1;
+    temporary2->next=head_start;
+    free(temporary3);
+    n=1;
+    temporary4=head_start;
+    printf("\nAfter deletion from the front, the circular doubly linked list is : \n");
+    do
+    {
+        printf(" <= %d =>",temporary4->data);
+        temporary4=temporary4->next;
+        n++;
+    }
+    while(temporary4!=head_start);
+    return 0;
+}
